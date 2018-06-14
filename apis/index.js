06d0aaafe6703955ae67b93085c340ca2cf025f4
elvis <elvis@lnmpy.com>
2018-06-14 14:33:59 +0800

    remove wechat api support

--- apis/index.js
+++ apis/index.js
@@ -3,4 +3,3 @@ exports.gfw_proxy = require('./gfw-proxy');
 exports.toby_proxy = require('./toby-proxy');
 exports.circleci = require('./circleci');
 exports.notify = require('./notify');
-exports.wechat = require('./wechat');
